95d1773e4ca8ad7add8294744ff2b524a167ff72
t00l13 <itollik@yandex.ru>
2022-10-07 15:30:47 +0300

    hotfix2

--- models/user.js
+++ models/user.js
@@ -7,18 +7,21 @@ const UnauthorizedError = require('../errors/UnauthorizedError');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
+    required: false,
     default: 'Жак-Ив Кусто',
     minlength: 2,
     maxlength: 30,
   },
   about: {
     type: String,
+    required: false,
     default: 'Исследователь',
     minlength: 2,
     maxlength: 30,
   },
   avatar: {
     type: String,
+    required: false,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     validate: {
       validator: validationLink,
