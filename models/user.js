80847c6bc88c83bd43f57c78550ae12ce3bc2b2f
t00l13 <itollik@yandex.ru>
2022-10-07 15:17:44 +0300

    hotfix

--- models/user.js
+++ models/user.js
@@ -22,7 +22,6 @@ const userSchema = new mongoose.Schema({
   avatar: {
     type: String,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
-    required: true,
     validate: {
       validator: validationLink,
       message: (props) => `${props.value} is not a valid link!`,
