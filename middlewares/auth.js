774c9bffc0e3f1b2ecd2fe0b0b2ffb8c36c73c87
t00l13 <itollik@yandex.ru>
2022-10-07 21:20:36 +0300

    hotfix3

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -17,7 +17,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
   } catch (err) {
-    throw new UnauthorizedError('Необходима авторизация');
+    next(new UnauthorizedError('Необходима авторизация!'));
   }
   req.user = payload;
 
